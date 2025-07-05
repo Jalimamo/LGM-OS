f15bd924cd6a2ef44b26f69f5e35fc6064faeddc
Jalimamo <jannick@mollien.org>
2025-07-05 17:42:54 +0200

    Changed printf to support multiple Calls and new Lines

--- kernel.cpp
+++ kernel.cpp
@@ -8,8 +8,34 @@
 void printf(char* str){
   static uint16_t* VideoMemory = (uint16_t*) 0xb8000;
 
+  static uint8_t x = 0, y = 0;
+
   for(int i = 0; str[i] != '\0'; ++i){
-    VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+
+
+    switch(str[i]) {
+      case '\n':
+        y++;x=0;
+        break;
+      default:
+        VideoMemory[80*y+x] = (VideoMemory[i] & 0xFF00) | str[i];
+        x++;
+    }
+
+
+    if(x >= 80){
+      x = 0;
+      y++;
+    }
+
+    if(y >= 25){
+      for(y=0;y>25;y++)
+        for(x=0;x<80;x++)
+          VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+
+
+      x = 0; y = 0;
+    }
   }
 }
 
@@ -24,7 +50,7 @@ extern "C" void callConstructors(){
 }
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /* multiboot magicnumber*/){
-  printf("FUER DIE LGM-RECHTSANWAELTE");
+  printf("FUER DIE LGM-RECHTSANWAELTE!!!\nUnd fuer die leuchtende Republik von Fluchs und Flamme!!!");
 
   GlobalDescriptorTable gdt;
 
