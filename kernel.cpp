92250ed88533221cfd080a2e5183981b26d3726b
Jalimamo <jannick@mollien.org>
2025-07-19 00:48:58 +0200

    Added new Interrupt-Setup to Main-Method

--- kernel.cpp
+++ kernel.cpp
@@ -55,5 +55,10 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /* multiboo
 
   GlobalDescriptorTable gdt;
 
+  InterruptManager interrupts(&gdt);
+
+  interrupts.Activate();
+
+
   while(1);
 }
